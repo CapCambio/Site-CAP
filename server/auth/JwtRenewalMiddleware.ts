5326b285e0562485a19f414937e4f0621ceda879
CAP Câmbio <capcambiocx@gmail.com>
2026-07-07 16:15:58 -0300

    Corrigir configuração de sameSite no JwtRenewalMiddleware para consistir com login

--- server/auth/JwtRenewalMiddleware.ts
+++ server/auth/JwtRenewalMiddleware.ts
@@ -36,7 +36,7 @@ export const jwtRenewalMiddleware = (req: Request, res: Response, next: NextFunc
       res.cookie('jwt', newToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
       });
 
